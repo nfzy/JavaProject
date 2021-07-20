a33e644b6ece4d04ca9ec290b8b397b42d2eabe3
nfzy <2422238042@qq.com>
2021-07-20 15:45:51 +0800

    Chapter 1 pratice

---
+++ C Prime Plus/1_1.c
@@ -0,0 +1,28 @@
+/*
+ *-----------------------------------------------------
+ *    Date: Tue Jul 20 10:08:02 AM CST 2021
+ *    Effect:
+ *          转换英寸和厘米的进制（1 inch = 2.54 cm）
+ *-----------------------------------------------------
+ *
+ * */
+#include <stdio.h>
+
+int main(void)
+{
+    double inch;
+    printf("Enter a number of inch: ");
+    printf("        \b\b\b\b\b\b\b\b");
+    scanf("%lf", &inch);
+    printf("The inch convert centermeter is: %.2lf\n", inch*2.54);
+    return 0;
+}
+
+/*
+ *  在gcc (GCC) 11.1.0下的结果为:
+ *---------------------------------------------------------
+ *  Enter a number of inch: 100
+ *  The inch convert centermeter is: 254.00
+ *---------------------------------------------------------
+ *
+ * */
