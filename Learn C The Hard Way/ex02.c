52a283ab1804f5356050961c2afe460901a17116
nfzy <2422238042@qq.com>
2021-08-07 13:25:37 +0800

    sort these files

---
+++ Learn C The Hard Way/ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 12;
+    int height = 180;
+    printf("I am %d years old.\n" , age);
+    printf("I am %d centermeter tall.\n" , height);
+
+    return 0;
+}
