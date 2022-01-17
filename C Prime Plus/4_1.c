ffc4b0ff1d859d88fd99f733639cd893178e6dd2
nfzy <2422238042@qq.com>
2022-01-17 12:29:30 +0800

    modified chapter four files

--- C Prime Plus/4_1.c
+++ C Prime Plus/4_1.c
@@ -1,3 +1,10 @@
+/*
+ *	日期:	2022年 01月 17日 星期一 11:52:10 CST
+ *
+ *  功能:	提示用户输入名和姓,然后以"名, 姓"的格式打印出来。
+ *
+ * */
+
 #include <stdio.h>
 
 int main(void)
@@ -8,7 +15,19 @@ int main(void)
     scanf("%s", last_name);
     printf("Enter first name: ");
     scanf("%s", first_name);
-    printf("%s , %s\n" ,last_name , first_name);
+    printf("%s, %s\n" ,last_name, first_name);
 
     return 0;
 }
+
+/*
+ *	在 gcc (GCC) 11.1.0下的结果为:
+ *------------------------------------------------------
+ *
+ *  Enter last name: Zhou
+ *  Enter first name: An
+ *  Zhou, An
+ *
+ *------------------------------------------------------
+ *
+ * */
